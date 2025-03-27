8e33f01a046b5595106b0806a7edbb36febf10a4
Alexander Tan <alexandertan527@gmail.com>
2025-03-27 00:04:02 +0000

    adding seed file

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem "devise"
 gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
+gem 'simple_table_for'
 gem "sassc-rails"
 
 group :development, :test do
--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,5 +1,20 @@
 class CandidatesController < ApplicationController
+  before_action :set_candidate, only: [:show]
+
   def index
-    @candidates = Candidate.all
+    @candidates = Candidate.order(id: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @candidate = Candidate.new
+  end
+
+  private
+
+  def set_candidate
+    @candidate = Candidate.find(params[:id])
   end
 end
