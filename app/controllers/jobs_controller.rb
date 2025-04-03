4e859983b06a751787e03faf7cf388b65a290aad
Alexander Tan <alexandertan527@gmail.com>
2025-04-03 15:53:36 +0100

    jobs index and show page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,16 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [:show]
 
   def index
     @jobs = Job.order(id: :desc)
   end
 
+  def show
+  end
+
+  private
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
 end
