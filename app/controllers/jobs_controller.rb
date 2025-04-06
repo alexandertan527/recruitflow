6a65e1ac90458e821fd2dfcbe565e4dd15bfaaca
Alexander Tan <alexandertan527@gmail.com>
2025-04-06 13:58:29 +0100

    jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :set_job, only: [:show]
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
 
   def index
     @jobs = Job.order(id: :desc)
@@ -8,9 +8,41 @@ class JobsController < ApplicationController
   def show
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+    if @job.save
+      redirect_to job_path(@job), notice: "Job was created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @job.update(job_params)
+    redirect_to job_path(@job)
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path
+  end
+
   private
 
   def set_job
     @job = Job.find(params[:id])
   end
+
+  def job_params
+    params.require(:job).permit(:job_title, :client_contact, :status, :is_live, :employment_type, :salary)
+  end
+
 end
