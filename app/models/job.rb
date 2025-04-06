26d85ef0c6db6ec595224d300b046c31633d2266
Alexander Tan <alexandertan527@gmail.com>
2025-04-06 14:09:48 +0100

    adding job model validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,7 @@
 class Job < ApplicationRecord
   belongs_to :client_contact
   belongs_to :user
+  validates :job_title, :status, :is_live, :employment_type, presence: :true
+  validates :status, inclusion: { in: ["Accepting Candidates", "Open", "On Hold", "Placed", "Closed-Lost"]}
+  validates :employment_type, inclusion: { in: ["Full-time", "Contract"]}
 end
