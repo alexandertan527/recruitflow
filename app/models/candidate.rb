e87ddb161874f66e9ebab0c50bc1d7b3360a8bc3
Alexander Tan <alexandertan527@gmail.com>
2025-04-06 15:29:55 +0100

    submission routes

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,5 +1,6 @@
 class Candidate < ApplicationRecord
   belongs_to :user
+  has_many :submissions
   validates :first_name, :last_name, :email, :status, presence: true
   validates :status, inclusion: { in: ["Active", "Available", "Interviewing", "Under Offer", "Pre-screened", "Placed"]}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,14 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :candidates
+  resources :candidates do
+    resources :submissions, only: [:show, :new, :create, :edit, :update, :destroy]
+  end
   resources :clients
   resources :client_contacts
-  resources :jobs
+  resources :jobs do
+    resources :submissions, only: [:show, :new, :create, :edit, :update, :destroy]
+  end
+  resources :submissions
 
 end
