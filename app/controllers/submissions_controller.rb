f7d849f96208098897e55e19b7488bcfec4d517c
Alexander Tan <alexandertan527@gmail.com>
2025-04-12 21:22:23 +0100

    submissions backend

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,6 @@
 class SubmissionsController < ApplicationController
   before_action :set_submission, only: [:show, :destroy]
+  before_action :find_candidate_or_job, only: [:new, :create]
 
   def index
     @submissions = Submission.all.order(id: :desc)
@@ -9,8 +10,24 @@ class SubmissionsController < ApplicationController
   end
 
   def new
-    @submission = Submission.new
+    @submission = Submission.new()
   end
+
+  def create
+    @submission = Submission.new(submission_params)
+    @submission.user = current_user
+
+    if @submission.save
+      if params[:candidate_id]
+        redirect_to candidate_path(@candidate), notice: "Submission was created successfully!"
+      elsif params[:job_id]
+        redirect_to job_path(@job), notice: "Submission was created successfully!"
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 
   def destroy
     @submission.destroy
@@ -19,11 +36,19 @@ class SubmissionsController < ApplicationController
 
   private
 
+  def submission_params
+    params.require(:submission).permit(:candidate_id, :job_id)
+  end
+
   def set_submission
     @submission = Submission.find(params[:id])
   end
 
-  def find_candidate
-    # @candidate = Candidate.find(params[:candidate_id])
+  def find_candidate_or_job
+    if params[:candidate_id]
+      @candidate = Candidate.find(params[:candidate_id])
+    elsif params[:job_id]
+      @job = Job.find(params[:job_id]) if params[:job_id] != nil
+    end
   end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,4 +2,5 @@ class Submission < ApplicationRecord
   belongs_to :job
   belongs_to :candidate
   belongs_to :user
+  validates :candidate, :job, presence: true
 end
