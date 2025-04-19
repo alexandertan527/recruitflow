4b0ba0f5d935ab04c8eac734ee171be7f1431554
Alexander Tan <alexandertan527@gmail.com>
2025-04-19 17:58:56 +0100

    homepage login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :demo_login ]
 
   def home
+  end
+
+  def demo_login
+    demo_user = User.find(1)
+    sign_in(demo_user)
+    redirect_to root_path, notice: "Signed in as demo user"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  post 'demo_login', to: 'pages#demo_login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
